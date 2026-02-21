4220b559e523c5068ba231a30f3fe5517f577e48
jrodriguezes <jeremy12troll@gmail.com>
2026-02-20 23:43:07 -0600

    esquema, dto, modulo, controlador y servicio de usuarios completo

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  create(@Body() dto: CreateUserDto) {
+    return this.usersService.create(dto);
+  }
+
+  @Get(':id')
+  findByIdWithHash(@Param('id') id: string) {
+    return this.usersService.findByIdWithHash(id);
+  }
+}
