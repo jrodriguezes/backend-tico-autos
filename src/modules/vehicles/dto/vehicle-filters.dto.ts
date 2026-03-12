4d1e0349429ba93f0909f147ea5eeb4ef7de8631
jrodriguezes <jeremy12troll@gmail.com>
2026-03-12 00:18:07 -0600

    Dto de filtros

---
+++ src/modules/vehicles/dto/vehicle-filters.dto.ts
@@ -0,0 +1,31 @@
+import { IsString, IsNumber, IsOptional } from 'class-validator';
+
+export class VehicleFiltersDto {
+  @IsOptional()
+  @IsString()
+  brand: string;
+
+  @IsOptional()
+  @IsString()
+  model: string;
+
+  @IsOptional()
+  @IsNumber()
+  minYear: number;
+
+  @IsOptional()
+  @IsNumber()
+  maxYear: number;
+
+  @IsOptional()
+  @IsNumber()
+  minPrice: number;
+
+  @IsOptional()
+  @IsNumber()
+  maxPrice: number;
+
+  @IsOptional()
+  @IsString()
+  status: string;
+}
